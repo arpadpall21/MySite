056a898cc8060601a695fc143ccf7d9a613de933
arpadpall21 <a.pall21ar@gmail.com>
2021-04-05 11:39:39 +0300

    2021-04-05/2

--- Assets/expressSrv/srvExpress.js
+++ Assets/expressSrv/srvExpress.js
@@ -2,18 +2,19 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-// console.log( express );
 app.use(
     cookieParser(),
     function(req, res, next){ 
         console.log( req.cookies );
         
-        res.cookie('srvCookie', 21, {httpOnly:true});
+        res.cookie('someCookie', 20);
+        res.cookie('httpOnlyCookie', 21, {httpOnly:true});
         
         next();
     },
-    express.static('../')
+    express.static('../../')
 )
 
 app.listen(1000, ()=>console.log('Express is listening on localhost:1000'));
+
 
