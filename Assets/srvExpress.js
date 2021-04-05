7c5a499d2b830f58b74f41fc264f0b00f6af52e5
arpadpall21 <a.pall21ar@gmail.com>
2021-04-05 09:13:11 +0300

    2021/04/05

--- Assets/srvExpress.js
+++ Assets/srvExpress.js
@@ -1,9 +1,19 @@
-const express = require('/usr/lib/node_modules/express');           // globally installed express package (for some FUCK I cannot use bare specifier WTF)
+const express = require('express');     
+const cookieParser = require('cookie-parser');
 const app = express();
 
+// console.log( express );
 app.use(
-    (req, res, next) => { console.log('req URL = ' + req.originalUrl); next() },
+    cookieParser(),
+    function(req, res, next){ 
+        console.log( req.cookies );
+        
+        res.cookie('srvCookie', 21, {httpOnly:true});
+        
+        next();
+    },
     express.static('../')
 )
 
 app.listen(1000, ()=>console.log('Express is listening on localhost:1000'));
+
