bff386c65520052397db2db344cdd98992b5c65b
pallarpad <a.pall21ar@gmail.com>
2021-07-27 18:39:26 +0200

    2021-07-27

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,10 +1,11 @@
-function throwErr(){
-  throw new Error('some error')
-}
-
-test('test-1', function(){
-  expect(throwErr()).toThrow()
-  expect(throwErr()).toThrow('some error')
+describe('desc-1', function(){
+  test('test', function(){})
 })
 
+describe.skip.each(['des-1', 'desc-2', 'desc-3'])('name: %s', function(){            // only this describe each is executed 
+  test('test', function(){})
+})
 
+describe('desc-3', function(){
+  test('test', function(){})
+})
