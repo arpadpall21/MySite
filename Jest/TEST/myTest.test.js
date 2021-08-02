e92bc54bf1b7aa5e925a01cc1a73987d68079292
pallarpad <a.pall21ar@gmail.com>
2021-08-02 08:52:33 +0200

    2021-08-02

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,9 +1,4 @@
-test('test-1', () => {})
-test('test-2', () => {})
-test('test-3', () => {})
-
-test.todo('we should have 4 tests')
-
-test('test-4', () => {})
-
-test.todo('ok we have 4 tests now')
+test('test', function(){
+  
+  expect('Hello Word').toEqual(expect.stringMatching('Hello'))
+})
