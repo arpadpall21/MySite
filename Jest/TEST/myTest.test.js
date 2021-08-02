57e305e977e4e43fb7a87a8b98a3f98f4760f539
arpadpall21 <a.pall21@yahoo.fr>
2021-08-02 21:36:10 +0200

    2021-08-02/2

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,4 +1,15 @@
-test('test', function(){
-  
-  expect('Hello Word').toEqual(expect.stringMatching('Hello'))
+expect.extend({
+  toBeNotString(received){
+    if(typeof received !== 'string'){
+      return {pass:true, message:() => 'not a string data type'}
+    } else {
+      return {pass:false, message:() => `${received} is a string data type`}
+    }
+  }
 })
+
+test('should not be a string', function(){
+  expect(21).toBeNotString()                  // test passes 
+  expect('21').toBeNotString()                // test fails 
+})
+
