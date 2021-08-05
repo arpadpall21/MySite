3737aa155131196adce59298bd3a74074bced773
arpadpall21 <a.pall21@yahoo.fr>
2021-08-05 08:51:50 +0200

    2021-08-05

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,24 +1,7 @@
-test('some test-1', function(){
-  expect('Kicsi kutya tarka').toEqual(expect.stringContaining('kutya'))   // test passes 
-  expect('Kicsi kutya tarka').toEqual(expect.stringContaining('macka'))   // test fails 
-})
-
-test('some test-2', function(){
-  expect('Kicsi kutya tarka').toEqual(expect.stringMatching('ta'))        // test passes 
-  expect('Kicsi kutya tarka').toEqual(expect.stringMatching(/k.cs.\s/i))  // test passes 
-  
-  expect('Kicsi kutya tarka').toEqual(expect.stringMatching('farka'))     // test fails
-  expect('Kicsi kutya tarka').toEqual(expect.stringMatching(/k.cs.\s/))   // test fails 
-})
-
-const mock = jest.fn()
-
-function testFn(a, b, cb){
-  cb(a, b)
-}
-
-test('mock called with', function(){
-  testFn('one', 'two', mock)
-  
-  expect(mock).toBeCalledWith(expect.stringContaining('one'), expect.stringContaining('two'))   // test passes 
-})
+describe('my beverage', () => {
+  test('is delicious and not sour', () => {
+    const myObj = {p1:21, p2:{nest:33}}
+    
+    expect([1, 2, {p1:21, nest:[11]}]).toContainEqual({p1:21, nest:[11]})
+  });
+});
