a0b002ee3fc12c22d0c35b1c3138bc80fd6fd900
arpadpall21 <a.pall21@yahoo.fr>
2021-08-09 08:50:25 +0200

    2021-08-09

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,7 +1,31 @@
-describe('my beverage', () => {
-  test('is delicious and not sour', () => {
-    const myObj = {p1:21, p2:{nest:33}}
+class TestClass {
+  constructor(a, b){
+    this.a = a
+    this.b = b;
+  }
+}
+
+var myObj = new TestClass(21, 22)
+
+// toEqual() --------------------------------------------------------------------------------
+  test('test-1', function(){
+    expect({}).toEqual({p1:undefined})                                                      // test passes 
     
-    expect([1, 2, {p1:21, nest:[11]}]).toContainEqual({p1:21, nest:[11]})
-  });
-});
+    expect([,1]).toEqual([undefined,1])                                                     // test passes 
+    
+    expect({a:21, b:22}).toEqual(myObj)
+  })
+  
+  
+// toStrictEqual() -------------------------------------------------------------------------
+  test('test-2', function(){
+    expect({}).toStrictEqual({p1:undefined})                                                // test fails  
+  })
+  
+  test('test-2', function(){
+    expect([,1]).toStrictEqual([undefined,1])                                               // test fails
+  })
+
+  test('test-3', function(){
+    expect({a:21, b:22}).toStrictEqual(myObj)                                               // test fails
+  })
