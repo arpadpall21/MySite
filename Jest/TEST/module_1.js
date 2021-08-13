8c3b9fd1451240bf53a2f08be3f6451c74824eda
arpadpall21 <a.pall21@yahoo.fr>
2021-08-13 06:14:23 +0200

    2021-08-13

--- Jest/TEST/module_1.js
+++ Jest/TEST/module_1.js
@@ -4,5 +4,7 @@ module.exports = {
   get10() { return 10 },
   p1:21,
   p2:[1, 2, 3],
-  p3:{n1:'x'}
+  p3:{n1:'x', n2(){ return 22}},
+  p4:{n2:'y'},
+  p5:{n1:{n2:{n3:{n4:{n5(){ return 'really deep!' }}}}}}
 }
--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,27 +1,27 @@
 const mod1 = require('./module_1')
-jest.mock('./module_1', () => {
-  return {
-    get2: null
-  }
-})
+jest.mock('./module_1')
 
 it('test', () => {
-  console.log( mod1.get2() )        // undefined 
+  console.log( mod1.get2 )        // undefined -> automatically mocked 
   console.log( mod1.getRandom() )   // undefined
   
   mod1.getRandom.mockImplementation(() => 21)   // mocking an imported function setting a default value to the imported function 
   console.log( mod1.getRandom() )               // -> 21
+    
+  console.log( mod1.p1 )                        // -> 21    // properties are not automatically mocked 
+  console.log( mod1.p2 )                        // -> []
+  console.log( mod1.p3.n2() )                   // -> undefined   // functions in any depth are mocked 
+  console.log( mod1.p4.n2 )
+  console.log( mod1.p5.n1.n2.n3.n4.n5() )       // -> undefined   // functions in any depth are mocked 
   
   
-  console.log( mod1.p1 )
-  console.log( mod1.p2 )
-  console.log( mod1.p3 )
+  // mod1.get10.mockImplementationOnce(() => 11 )
+  // console.log( mod1.get10() )                   // -> 11
+  // console.log( mod1.get10() )                   // -> undefined 
   
-  mod1.get10.mockImplementationOnce(() => 11 )
-  console.log( mod1.get10() )                   // -> 11
-  console.log( mod1.get10() )                   // -> undefined 
+  // mod1.getRandom.mockReturnThis()
+  // console.log( getRandom() )
   
-  mod1.getRandom.mockReturnThis()
-  console.log( getRandom() )
+  const mock
 })
 
