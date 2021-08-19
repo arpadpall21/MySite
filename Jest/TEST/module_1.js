e96700ea132ade9f554a9d9f90b56d97edbeecde
pallarpad <a.pall21ar@gmail.com>
2021-08-19 17:29:10 +0200

    2021-08-19

--- Jest/TEST/module_1.js
+++ Jest/TEST/module_1.js
@@ -1,24 +1,38 @@
-class TestClass {
-  constructor(a, b){
-    this.a = a
-    this.b = b
-  }
+// class TestClass {
+//   constructor(a, b){
+//     this.a = a
+//     this.b = b
+//   }
   
-  summAll(){
-    return a + b
+//   summAll(){
+//     return a + b
+//   }
+// }
+
+// module.exports = {
+//   get2() { return 2 },
+//   getRandom() { return Math.random * 10 },
+//   get10() { return 10 },
+//   p1:21,
+//   p2:[1, 2, 3],
+//   p3:{n1:'x', n2(){ return 22}, n3:22, n4:[1, 2, 3], n5:false},
+//   p4:{n2:'y'},
+//   p5:{n1:{n2:{n3:{n4:{n5(){ return 'really deep!' }}}}}},
+//   myClass: TestClass
+// }
+
+// console.log( 'required!' )
+
+
+async function testPromise() {
+  try {
+    throw new Error('some error')
+  } catch (e) {
   }
+  // return 'test'
 }
 
-module.exports = {
-  get2() { return 2 },
-  getRandom() { return Math.random * 10 },
-  get10() { return 10 },
-  p1:21,
-  p2:[1, 2, 3],
-  p3:{n1:'x', n2(){ return 22}, n3:22, n4:[1, 2, 3], n5:false},
-  p4:{n2:'y'},
-  p5:{n1:{n2:{n3:{n4:{n5(){ return 'really deep!' }}}}}},
-  myClass: TestClass
-}
+testPromise()
+  .then((res) => console.log(res))
+  .catch((rej) => console.log(rej))
 
-console.log( 'required!' )
