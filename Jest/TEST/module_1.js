1cb74b21a6ea084a9781c8be6e0559d81316a08c
pallarpad <a.pall21ar@gmail.com>
2021-08-22 14:35:06 +0200

    2021-08-22

--- Jest/TEST/module_1.js
+++ Jest/TEST/module_1.js
@@ -1,38 +1,14 @@
-// class TestClass {
-//   constructor(a, b){
-//     this.a = a
-//     this.b = b
-//   }
-  
-//   summAll(){
-//     return a + b
-//   }
-// }
-
-// module.exports = {
-//   get2() { return 2 },
-//   getRandom() { return Math.random * 10 },
-//   get10() { return 10 },
-//   p1:21,
-//   p2:[1, 2, 3],
-//   p3:{n1:'x', n2(){ return 22}, n3:22, n4:[1, 2, 3], n5:false},
-//   p4:{n2:'y'},
-//   p5:{n1:{n2:{n3:{n4:{n5(){ return 'really deep!' }}}}}},
-//   myClass: TestClass
-// }
-
-// console.log( 'required!' )
-
-
-async function testPromise() {
-  try {
-    throw new Error('some error')
-  } catch (e) {
+module.exports = class Animal {
+  constructor(race, wild){
+    this.race = race;
+    this.wild = wild;
   }
-  // return 'test'
+  
+  getAnimal(){
+    return {
+      race: this.race,
+      wild: this.wild
+    }
+  }
 }
-
-testPromise()
-  .then((res) => console.log(res))
-  .catch((rej) => console.log(rej))
 
--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,19 +1,56 @@
-test('some-test', function(){
-  const mockFn = jest.fn(() => 21);
-  console.log( mockFn() );                        // -> 21
+test('some-test', async function(){
+// .mockClear() / jest.crearAllMocks() --------------------------------
+  const mockFn1 = jest.fn((a, b) => a + b);
+  const mockFn2 = jest.fn((a, b) => a + b);
+  const mockFn3 = jest.fn((a, b) => a + b);
   
-  mockFn.mockImplementation(() => 22);            // overrides the previous optional mock implementation 
-  console.log( mockFn() );                        // -> 22
+  mockFn1(1, 2)
+  mockFn2(5, 5)
+  mockFn3(7, 4)
   
-// using with classes ----------------------------
-  class Animal {
-    constructor(type, wild){
-      this.type = type
-      this.wild = wild
-    }
-    
-    getAnimal(){
-      return 
-    }
-  }
+  console.log( mockFn1.mock.calls );                                   // -> [[1, 2]]                    
+  console.log( mockFn2.mock.results );                                 // -> [{type:'return', value:10}] 
+  console.log( mockFn3.mock.results );                                 // -> [{type:'return', value:11}] 
+  
+  mockFn1.mockClear();                                                 // clears the 'mock.calls' and mock.results' for this mock function only 
+  
+  console.log( mockFn1.mock.calls );                                   // -> [] 
+  console.log( mockFn2.mock.results );                                 // -> [{type:'return', value:10}] 
+  console.log( mockFn3.mock.results );                                 // -> [{type:'return', value:11}] 
+  
+  jest.clearAllMocks();                                                // clears the 'mock.calls' and mock.results' for all mock functions 
+  
+  console.log( mockFn1.mock.calls );                                   // -> [] 
+  console.log( mockFn2.mock.results );                                 // -> [] 
+  console.log( mockFn3.mock.results );                                 // -> []   
+  
+  console.log( mockFn1(1, 2) )                                         // -> 3              // does not remove any mock implementation 
+  
+// .mockReset() / jest.resetAllMocks() --------------------------------
+  const mockFn4 = jest.fn((a, b) => a + b);
+  const mockFn5 = jest.fn((a, b) => a + b);
+  
+  mockFn4(1, 2)
+  mockFn5(5, 5)
+  
+  console.log( mockFn4.mock.calls );                                   // -> [[1, 2]]                    
+  console.log( mockFn5.mock.results );                                 // -> [{type:'return', value:10}] 
+  
+  mockFn4.mockReset();                                                 // clears the 'mock.calls' and mock.results' and also resets all mock implementations for this mock function only   
+  
+  console.log( mockFn4.mock.calls );                                   // -> []
+  console.log( mockFn5.mock.results );                                 // -> [{type:'return', value:10}]
+  console.log( mockFn4(1, 2) );                                        // -> undefined        // because mock implementations are cleared as well 
+  console.log( mockFn5(5, 5) );                                        // -> 10 
+  
+  jest.resetAllMocks();                                                // clears the 'mock.calls' and mock.results' and also resets all mock implementations for all mock functions    
+  
+  console.log( mockFn4.mock.calls );                                   // -> []
+  console.log( mockFn5.mock.results );                                 // -> []
+  console.log( mockFn4(1, 2) );                                        // -> undefined        // because mock implementations are cleared as well 
+  console.log( mockFn5(5, 5) );                                        // -> undefined  
+  
+  
+  
+  
 })
