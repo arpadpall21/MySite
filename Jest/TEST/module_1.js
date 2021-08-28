5515262698299f620c1302198e9c5bc18ddd8747
pallarpad <a.pall21ar@gmail.com>
2021-08-28 20:38:07 +0200

    2021-08-28

--- Jest/TEST/module_1.js
+++ Jest/TEST/module_1.js
@@ -1,14 +1,3 @@
-module.exports = class Animal {
-  constructor(race, wild){
-    this.race = race;
-    this.wild = wild;
-  }
-  
-  getAnimal(){
-    return {
-      race: this.race,
-      wild: this.wild
-    }
-  }
+module.exports = function(){
+  return 'default'
 }
-
--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,36 +1,16 @@
-test('some test', function(){
-  const mockFn = jest.fn(function(shouldThrow){
-    if (shouldThrow) {
-      throw new Error('some error');
-    } else {
-      return 'something';
-    }
-  })
-  
-  mockFn();
-  try { mockFn(true); } catch(e) { }
-  mockFn();
-  
-// toHaveReturned() / toHaveReturnedTimes() ------------------
-  expect(mockFn).toHaveReturned();                            // mock function returned (not throw) at least once 
-  expect(mockFn).toHaveReturnedTimes(2);                      // mock function returned 2 times (of of 3 calls throws once)
-  
-// toHaveReturnedWith() / toHaveLastReturnedWith() / toHaveNthReturnedWith() ---
-  let counter = 0;
-  const jestFn2 = jest.fn(function(){
-    counter++;
-    return counter;
-  })
-  
-  jestFn2();
-  jestFn2();
-  jestFn2();
-  
-  expect(jestFn2).toHaveReturnedWith(2);                      // mock function have returned 2 at lest once 
-  
-  expect(jestFn2).toHaveLastReturnedWith(3);                  // mock function have returned 3 for the last time  
-  
-  expect(jestFn2).toHaveNthReturnedWith(1, 1);                // mock function have returned 1 for the fist time 
-  expect(jestFn2).toHaveNthReturnedWith(2, 2);                // mock function have returned 2 for the 2nd time 
-  expect(jestFn2).toHaveNthReturnedWith(3, 3);                // mock function have returned 3 for the 3rd time 
+jest.enableAutomock();                                                    // enables module auto mocking (enable by default from config)
+const importedFn = require('./module_1');                                 // imported function is automatically mocked   
+
+test('some test', () => {
+  console.log( importedFn() );                                            // -> undefined 
+
 })
+
+jest.enableAutomock();                                                    // enables module auto mocking (enable by default from config)
+const importedFn2 = require('./module_1');                                // imported function is not mocked anymore
+
+test('some test', () => {
+  console.log( importedFn2() );                                           // -> undefined 
+
+})
+
