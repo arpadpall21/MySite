8174da762497755d65419d65bdc46325214293f3
arpadpall21 <a.pall21@yahoo.fr>
2021-08-28 09:26:21 +0200

    2021-08-28

--- Jest/TEST/myTest.test.js
+++ Jest/TEST/myTest.test.js
@@ -1,40 +1,36 @@
 test('some test', function(){
-  const myObj = { 
-    add(a, b){ return a + b},
-    multiply(a, b){ return a * b}
-  }
+  const mockFn = jest.fn(function(shouldThrow){
+    if (shouldThrow) {
+      throw new Error('some error');
+    } else {
+      return 'something';
+    }
+  })
   
-  const myObj2 = {
-    divide(a, b) { return a / b }
-  }
+  mockFn();
+  try { mockFn(true); } catch(e) { }
+  mockFn();
   
-  const spy1 = jest.spyOn(myObj, 'add'); 
-  const spy2 = jest.spyOn(myObj, 'multiply'); 
-  const spy3 = jest.spyOn(myObj2, 'divide'); 
+// toHaveReturned() / toHaveReturnedTimes() ------------------
+  expect(mockFn).toHaveReturned();                            // mock function returned (not throw) at least once 
+  expect(mockFn).toHaveReturnedTimes(2);                      // mock function returned 2 times (of of 3 calls throws once)
   
-  myObj.add(3, 3);
-  myObj.multiply(3, 3);
-  myObj2.divide(3, 3);
+// toHaveReturnedWith() / toHaveLastReturnedWith() / toHaveNthReturnedWith() ---
+  let counter = 0;
+  const jestFn2 = jest.fn(function(){
+    counter++;
+    return counter;
+  })
   
-  console.log( spy1.mock.calls );
-  console.log( spy2.mock.calls );
-  console.log( spy3.mock.calls );
+  jestFn2();
+  jestFn2();
+  jestFn2();
   
-  myObj.add(3, 3);
-  myObj.multiply(3, 3);
-  myObj2.divide(3, 3);
+  expect(jestFn2).toHaveReturnedWith(2);                      // mock function have returned 2 at lest once 
   
-  jest.restoreAllMocks();
-
-  myObj.add(3, 3);
-  myObj.multiply(3, 3);
-  myObj2.divide(3, 3);
-  myObj.add(3, 3);
-  myObj.multiply(3, 3);
-  myObj2.divide(3, 3);
+  expect(jestFn2).toHaveLastReturnedWith(3);                  // mock function have returned 3 for the last time  
   
-  console.log( spy1.mock.calls );
-  console.log( spy2.mock.calls );
-  console.log( spy3.mock.calls );
-  
+  expect(jestFn2).toHaveNthReturnedWith(1, 1);                // mock function have returned 1 for the fist time 
+  expect(jestFn2).toHaveNthReturnedWith(2, 2);                // mock function have returned 2 for the 2nd time 
+  expect(jestFn2).toHaveNthReturnedWith(3, 3);                // mock function have returned 3 for the 3rd time 
 })
